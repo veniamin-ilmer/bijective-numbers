1dbd547bd65bce94f6bf9f55c38d4338f46f10a9
Veniamin Ilmer <39828997+veniamin-ilmer@users.noreply.github.com>
2018-11-07 08:34:51 -0600

    Update based on experiment 1

--- bij.rs
+++ bij.rs
@@ -16,10 +16,9 @@ pub fn main() {
 
   let mut num1 = Bij::from(0_u64);
   
-  //Regular add = 1.2 seconds
-  for _ in 0..20000000 {   //16 secs
+  for _ in 0..20000000 {
     let num2 = Bij::from(rng.gen_range(1, 10));
-    num1 = num1 + num2;
+    num1 = &num1 + &num2;
   }
   let temp = u64::from(num1);
   println!("{}", temp);
@@ -83,25 +82,26 @@ use std::ops::Add;
 impl Add<Bij> for Bij {
   type Output = Bij;
   fn add(self, other: Bij) -> Bij {
-    let bigger;
-    let smaller;
     if self.mem.len() >= other.mem.len() {
-      bigger = self;
-      smaller = other;
+      self + &other  //Calls self + &other
     } else {
-      bigger = other;
-      smaller = self;
+      other + &self  //Calls other + &self
     }
-    bigger + &smaller  //Calls bigger + &smaller
   }
 }
 
 //&a + b = c
-//Consume b, replace with c.
 impl<'a> Add<Bij> for &'a Bij {
   type Output = Bij;
   fn add(self, other: Bij) -> Bij {
-    other + self   //Calls other + &self
+    //Per Experiment 1:
+    //We should only reuse the memory if the reusable variable is bigger.
+    //If the only reusable variable is smaller, clone the bigger variable instead.
+    if self.mem.len() < other.mem.len() {
+      other + self    //Calls other + &self
+    } else {
+      (*self).clone() + &other   //Calls self + &other
+    }
   }
 }
 
@@ -113,72 +113,56 @@ impl<'a> Add<&'a Bij> for Bij {
   type Output = Bij;
   fn add(self, other: &'a Bij) -> Bij {
     
-    let mut out = self;
-    
-    let bigger_len;
-    let smaller_len;
-    if out.mem.len() >= other.mem.len() {
-      bigger_len = out.mem.len();
-      smaller_len = other.mem.len();
-    } else {
-      bigger_len = other.mem.len();
-      smaller_len = out.mem.len();
+    //Per Experiment 1:
+    //We should only reuse the memory if the reusable variable is bigger.
+    //If the only reusable variable is smaller, clone the bigger variable instead.
+    if self.mem.len() < other.mem.len() {
+      //Calls this same function in reverse
+      return (*other).clone() + &self;  //Calls other + &self
     }
-    
+
+    let mut bigger = self;
+    let smaller = other;
+    //Beyond this point, `bigger` will be bigger in memory length, than `smaller`
+
     let mut carry = 0u8;
-    for i in 0..smaller_len {
-      match (out.mem[i], other.mem[i], carry) {
-        (false, false, 0) => out.mem[i] = true, //, carry = 0;     1+1+0= 2
-        (false, false, 1) => {}, //out[i] = false, carry = 1   1+1+1=11
-        (false, false, 2) => {out.mem[i] = true; carry = 1;},    //1+1+2=12
-        (true, false, 0) => {out.mem[i] = false; carry = 1;},    //2+1+0=11
-        (true, false, 1) => {}, //out[i] = true, carry = 1     2+1+1=12
-        (true, false, 2) => out.mem[i] = false, //carry = 2        2+1+2=21
-        (false, true, 0) => carry = 1,    //out[i] = false     1+2+0=11
-        (false, true, 1) => out.mem[i] = true, //carry = 1         1+2+1=12
-        (false, true, 2) => {}, //num[i] = false, carry = 2     1+2+2=21
-        (true, true, 0) => carry = 1, // out[i] = true         2+2+0=12
-        (true, true, 1) => {out.mem[i] = false; carry = 2;},     //2+2+1=21
-        (true, true, 2) => {}, //out[i] = true; carry = 2      2+2+2=22
-        _ => panic!("Unknown add combination {} {} {}", out.mem[i], other.mem[i], carry),
+    for i in 0..smaller.mem.len() {
+      match (bigger.mem[i], smaller.mem[i], carry) {
+        (false, false, 0) => bigger.mem[i] = true, //, carry = 0;   1+1+0= 2
+        (false, false, 1) => {}, //bigger[i] = false, carry = 1     1+1+1=11
+        (false, false, 2) => {bigger.mem[i] = true; carry = 1;},  //1+1+2=12
+        (true, false, 0) => {bigger.mem[i] = false; carry = 1;},  //2+1+0=11
+        (true, false, 1) => {}, //bigger[i] = true, carry = 1       2+1+1=12
+        (true, false, 2) => bigger.mem[i] = false, //carry = 2      2+1+2=21
+        (false, true, 0) => carry = 1,    //bigger[i] = false       1+2+0=11
+        (false, true, 1) => bigger.mem[i] = true, //carry = 1       1+2+1=12
+        (false, true, 2) => {}, //num[i] = false, carry = 2         1+2+2=21
+        (true, true, 0) => carry = 1, // bigger[i] = true           2+2+0=12
+        (true, true, 1) => {bigger.mem[i] = false; carry = 2;},   //2+2+1=21
+        (true, true, 2) => {}, //bigger[i] = true; carry = 2        2+2+2=22
+        _ => panic!("Unknown add combination {} {} {}", bigger.mem[i], smaller.mem[i], carry),
       }
     }
   
-    //The output is longer than the other vec, so we should only continue adding till there's no carry.
-    if out.mem.len() >= other.mem.len() {
-      for i in smaller_len..bigger_len {
-        if carry == 0 {
-          break;
-        }
-        match (out.mem[i], carry) {
-          (false, 1) => {out.mem[i] = true; carry = 0;},  //1+1=2
-          (false, 2) => {out.mem[i] = false; carry = 1;}, //1+2=11
-          (true, 1) => out.mem[i] = false, //carry = 1      2+1=11
-          (true, 2) => {out.mem[i] = true; carry = 1;},   //2+2=12
-          _ => panic!("Unknown add combination {} {}", out.mem[i], carry),
-        }
+    for i in smaller.mem.len()..bigger.mem.len() {
+      if carry == 0 {
+        break;
       }
-    //The other vec is longer than the output, so we must run through the whole vec.
-    } else {
-      for i in smaller_len..bigger_len {
-        match (other.mem[i], carry) {
-          (false, 0) => out.mem.push(false), //carry = 0;    1+0=1
-          (false, 1) => {out.mem.push(true); carry = 0;},  //1+1=2
-          (false, 2) => {out.mem.push(false); carry = 1;}, //1+2=11
-          (true, 0) => out.mem.push(true), //carry = 0       2+0=2
-          (true, 1) => out.mem.push(false), //carry = 1      2+1=11
-          (true, 2) => {out.mem.push(true); carry = 1;},   //2+2=12
-          _ => panic!("Unknown add combination {} {}", other.mem[i], carry),
-        }
+      match (bigger.mem[i], carry) {
+        (false, 1) => {bigger.mem[i] = true; carry = 0;},  //1+1=2
+        (false, 2) => {bigger.mem[i] = false; carry = 1;}, //1+2=11
+        (true, 1) => bigger.mem[i] = false, //carry = 1      2+1=11
+        (true, 2) => {bigger.mem[i] = true; carry = 1;},   //2+2=12
+        _ => panic!("Unknown add combination {} {}", bigger.mem[i], carry),
       }
     }
-    
+
     //Ran out of numbers, and there is still a carry
     if carry == 1 {
-      out.mem.push(false);
+      bigger.mem.push(false);
     }
   
-    out
+    bigger
   }
 }
 
@@ -187,16 +171,10 @@ impl<'a> Add<&'a Bij> for Bij {
 impl<'a> Add<&'a Bij> for &'a Bij {
   type Output = Bij;
   fn add(self, other: &'a Bij) -> Bij {
-    let bigger;
-    let smaller;
     if self.mem.len() >= other.mem.len() {
-      bigger = self;
-      smaller = other;
+      (*self).clone() + other  //Calls self + &other
     } else {
-      bigger = other;
-      smaller = self;
+      (*other).clone() + self  //Calls other + &self
     }
-
-    (*bigger).clone() + smaller  //Calls bigger + &smaller
   }
 }
