73e5202bd763f76ec882f332954d51e4cc4c4199
Veniamin Ilmer <39828997+veniamin-ilmer@users.noreply.github.com>
2018-11-08 12:40:24 -0600

    Macroed all adds. They run a little faster that way.

--- bij.rs
+++ bij.rs
@@ -17,7 +17,7 @@ pub fn main() {
   
   for _ in 0..20000000 {
     let num2 = Bij::from(rng.gen_range(1, 10));
-    num1 += &num2;
+    num1 = num1 + num2;
   }
 
   let temp = u64::from(num1);
@@ -36,50 +36,55 @@ impl Bij {
   fn new() -> Bij {
     Bij { mem: Vec::new() }
   }
+}
   
-  //This is the most efficient input form. All other additions use this one.
-  //Assumption: `bigger` memory length must be >= `smaller` memory length.
-  //All locations that call this function *must* take this into account before calling.
-  fn _add(bigger: &mut Bij, smaller: &Bij) {
-
+//Assumption: `bigger` memory length must be >= `smaller` memory length.
+//All locations that call this macro *must* take this into account before calling.
+//Per Experiment 1:
+//We should only reuse the memory if the reusable variable is bigger.
+//If the only reusable variable is smaller, clone the bigger variable instead.
+macro_rules! add {
+  //Overrights $bigger. Read only $smaller
+  ($bigger:expr, $smaller:expr) => (
     let mut carry = 0u8;
-    for i in 0..smaller.mem.len() {
-      match (bigger.mem[i], smaller.mem[i], carry) {
-        (false, false, 0) => bigger.mem[i] = true, //, carry = 0;   1+1+0= 2
+    for i in 0..$smaller.mem.len() {
+      match ($bigger.mem[i], $smaller.mem[i], carry) {
+        (false, false, 0) => $bigger.mem[i] = true, //, carry = 0;   1+1+0= 2
         (false, false, 1) => {}, //bigger[i] = false, carry = 1     1+1+1=11
-        (false, false, 2) => {bigger.mem[i] = true; carry = 1;},  //1+1+2=12
-        (true, false, 0) => {bigger.mem[i] = false; carry = 1;},  //2+1+0=11
+        (false, false, 2) => {$bigger.mem[i] = true; carry = 1;},  //1+1+2=12
+        (true, false, 0) => {$bigger.mem[i] = false; carry = 1;},  //2+1+0=11
         (true, false, 1) => {}, //bigger[i] = true, carry = 1       2+1+1=12
-        (true, false, 2) => bigger.mem[i] = false, //carry = 2      2+1+2=21
+        (true, false, 2) => $bigger.mem[i] = false, //carry = 2      2+1+2=21
         (false, true, 0) => carry = 1,    //bigger[i] = false       1+2+0=11
-        (false, true, 1) => bigger.mem[i] = true, //carry = 1       1+2+1=12
+        (false, true, 1) => $bigger.mem[i] = true, //carry = 1       1+2+1=12
         (false, true, 2) => {}, //num[i] = false, carry = 2         1+2+2=21
         (true, true, 0) => carry = 1, // bigger[i] = true           2+2+0=12
-        (true, true, 1) => {bigger.mem[i] = false; carry = 2;},   //2+2+1=21
+        (true, true, 1) => {$bigger.mem[i] = false; carry = 2;},   //2+2+1=21
         (true, true, 2) => {}, //bigger[i] = true; carry = 2        2+2+2=22
-        _ => panic!("Unknown add combination {} {} {}", bigger.mem[i], smaller.mem[i], carry),
+        _ => panic!("Unknown add combination {} {} {}", $bigger.mem[i], $smaller.mem[i], carry),
       }
     }
   
-    for i in smaller.mem.len()..bigger.mem.len() {
+    for i in $smaller.mem.len()..$bigger.mem.len() {
       if carry == 0 {
         break;
       }
-      match (bigger.mem[i], carry) {
-        (false, 1) => {bigger.mem[i] = true; carry = 0;},  //1+1=2
-        (false, 2) => {bigger.mem[i] = false; carry = 1;}, //1+2=11
-        (true, 1) => bigger.mem[i] = false, //carry = 1      2+1=11
-        (true, 2) => {bigger.mem[i] = true; carry = 1;},   //2+2=12
-        _ => panic!("Unknown add combination {} {}", bigger.mem[i], carry),
+      match ($bigger.mem[i], carry) {
+        (false, 1) => {$bigger.mem[i] = true; carry = 0;},  //1+1=2
+        (false, 2) => {$bigger.mem[i] = false; carry = 1;}, //1+2=11
+        (true, 1) => $bigger.mem[i] = false, //carry = 1      2+1=11
+        (true, 2) => {$bigger.mem[i] = true; carry = 1;},   //2+2=12
+        _ => panic!("Unknown add combination {} {}", $bigger.mem[i], carry),
       }
     }
 
     //Ran out of numbers, and there is still a carry
     if carry == 1 {
-      bigger.mem.push(false);
+      $bigger.mem.push(false);
     }
-  }
+  );
 }
+
 
 impl Clone for Bij {
     fn clone(&self) -> Bij {
@@ -124,10 +129,10 @@ use std::ops::AddAssign;
 impl<'a> AddAssign<&'a Bij> for Bij {
   fn add_assign(&mut self, other: &'a Bij) {
     if self.mem.len() >= other.mem.len() {
-      Bij::_add(self, other);
+      add!(self, other);
     } else {
       let mut cloned_other = (*other).clone();
-      Bij::_add(&mut cloned_other, self);
+      add!(cloned_other, self);
       *self = cloned_other;
     }
   }
@@ -137,9 +142,9 @@ impl<'a> AddAssign<&'a Bij> for Bij {
 impl AddAssign for Bij {
   fn add_assign(&mut self, mut other: Bij) {
     if self.mem.len() >= other.mem.len() {
-      Bij::_add(self, &other);
+      add!(self, other);
     } else {
-      Bij::_add(&mut other, self);
+      add!(other, self);
       *self = other;
     }
   }
@@ -152,15 +157,12 @@ use std::ops::Add;
 impl<'a> Add<&'a Bij> for Bij {
   type Output = Bij;
   fn add(mut self, other: &'a Bij) -> Bij {
-    //Per Experiment 1:
-    //We should only reuse the memory if the reusable variable is bigger.
-    //If the only reusable variable is smaller, clone the bigger variable instead.
     if self.mem.len() >= other.mem.len() {
-      Bij::_add(&mut self, other);
+      add!(self, other);
       self
     } else {
       let mut other = (*other).clone();
-      Bij::_add(&mut other, &self);
+      add!(other, self);
       other
     }
   }
@@ -172,10 +174,10 @@ impl Add<Bij> for Bij {
   type Output = Bij;
   fn add(mut self, mut other: Bij) -> Bij {
     if self.mem.len() >= other.mem.len() {
-      Bij::_add(&mut self, &other);
+      add!(self, other);
       self
     } else {
-      Bij::_add(&mut other, &self);
+      add!(other, self);
       other
     }
   }
@@ -185,16 +187,12 @@ impl Add<Bij> for Bij {
 impl<'a> Add<Bij> for &'a Bij {
   type Output = Bij;
   fn add(self, mut other: Bij) -> Bij {
-    
-    //Per Experiment 1:
-    //We should only reuse the memory if the reusable variable is bigger.
-    //If the only reusable variable is smaller, clone the bigger variable instead.
     if self.mem.len() >= other.mem.len() {
       let mut cloned_self = (*self).clone();
-      Bij::_add(&mut cloned_self, &other);
+      add!(cloned_self, other);
       cloned_self
     } else {
-      Bij::_add(&mut other, self);
+      add!(other, self);
       other
     }
   }
@@ -207,13 +205,12 @@ impl<'a> Add<&'a Bij> for &'a Bij {
   fn add(self, other: &'a Bij) -> Bij {
     if self.mem.len() >= other.mem.len() {
       let mut cloned_self = (*self).clone();
-      Bij::_add(&mut cloned_self, other);
+      add!(cloned_self, other);
       cloned_self
     } else {
       let mut cloned_other = (*other).clone();
-      Bij::_add(&mut cloned_other, self);
+      add!(cloned_other, self);
       cloned_other
     }
   }
 }
-
